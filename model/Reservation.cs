e5fb8cabbab1ba3e56e87c45dae2de84a573c4dc
Nistor Ioan-Razvan <149099252+razv11@users.noreply.github.com>
2024-04-16 18:51:18 +0300

    Add files via upload

---
+++ model/Reservation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace model
+{
+    [Serializable]
+    public class Reservation : Entity<long>
+    {
+        public string ClientName { get; set; }
+        public string PhoneNumber { get; set; }
+        public int NumberOfTickets { get; set; }
+        public User AgencyUser { get; set; }
+        public Trip CurrentTrip { get; set; }
+
+        public Reservation(string clientName, string phoneNumber, int numberOfTickets, User agencyUser, Trip trip)
+        {
+            ClientName = clientName;
+            PhoneNumber = phoneNumber;
+            NumberOfTickets = numberOfTickets;
+            AgencyUser = agencyUser;
+            CurrentTrip = trip;
+        }
+
+        public override string ToString()
+        {
+            return "ID: " + Id + " | Client name: " + ClientName;
+        }
+    }
+}
---
+++ model/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace model
+{
+    [Serializable]
+    public class User : Entity<long>
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public override string ToString()
+        {
+            return "ID: " + Id + " | Username: " + Username;
+        }
+    }
+}
