4b38144bfab03a9f8412f09bda3095b569dc7bde
Nistor Ioan-Razvan <149099252+razv11@users.noreply.github.com>
2024-04-16 18:49:02 +0300

    Add files via upload

---
+++ client/TravelAgencyUserEventArgs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace client
+{
+    public enum TravelAgencyUserEvent
+    {
+        TripReserved
+    }
+    
+    public class TravelAgencyUserEventArgs : EventArgs
+    {
+        private readonly TravelAgencyUserEvent _userEvent;
+        private readonly object _data;
+
+        public TravelAgencyUserEventArgs(TravelAgencyUserEvent userEvent, object data)
+        {
+            _userEvent = userEvent;
+            _data = data;
+        }
+
+        public TravelAgencyUserEvent EventType
+        {
+            get
+            {
+                return _userEvent;
+            }
+        }
+
+        public object Data
+        {
+            get
+            {
+                return _data;
+            }
+        }
+    }
+}
