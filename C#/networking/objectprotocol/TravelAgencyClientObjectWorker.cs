bcc07b79f6d1b697e7920c2cefcd0912973bfe6a
Nistor Ioan-Razvan <149099252+razv11@users.noreply.github.com>
2024-06-09 17:35:13 +0300

    Add files via upload

---
+++ C#/networking/objectprotocol/TravelAgencyClientObjectWorker.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
+using model;
+using networking.dto;
+using services;
+
+namespace networking.objectprotocol
+{
+    public class TravelAgencyClientObjectWorker : ITravelAgencyObserver
+    {
+        private ITravelAgencyServices _server;
+        private TcpClient _connection;
+
+        private NetworkStream _stream;
+        private IFormatter _formatter;
+        private volatile bool _connected;
+
+        public TravelAgencyClientObjectWorker(ITravelAgencyServices server, TcpClient connection)
+        {
+            _server = server;
+            _connection = connection;
+
+            try
+            {
+                _stream = connection.GetStream();
+                _formatter = new BinaryFormatter();
+                _connected = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+
+        private void SendResponse(IResponse response)
+        {
+            Console.WriteLine("Sending response: " + response);
+            lock (_stream)
+            {
+                _formatter.Serialize(_stream, response);
+                _stream.Flush();
+            }
+        }
+        
+        public void TripReserved(Reservation reservation)
+        {
+            Console.WriteLine("Book reserved: " + reservation);
+            try
+            {
+                SendResponse(new TripReservedResponse(reservation));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+
+        private IResponse HandleRequest(IRequest request)
+        {
+            if (request is LoginRequest)
+            {
+                Console.WriteLine("Received login request..");
+                LoginRequest loginRequest = (LoginRequest)request;
+                User currentUser = loginRequest.User;
+
+                try
+                {
+                    lock (_server)
+                    {
+                        _server.Login(currentUser, this);
+                    }
+
+                    return new OkResponse();
+                }
+                catch (Exception e)
+                {
+                    _connected = false;
+                    return new ErrorResponse(e.Message);
+                }
+            }
+
+            if (request is ReserveTripRequest)
+            {
+                Console.WriteLine("Received reserve trip request..");
+                ReserveTripRequest reserveTripRequest = (ReserveTripRequest)request;
+                Reservation reservation = reserveTripRequest.Reservation;
+
+                try
+                {
+                    lock (_server)
+                    {
+                        _server.ReserveTrip(reservation);
+                    }
+
+                    return new OkResponse();
+                }
+                catch (Exception e)
+                {
+                    return new ErrorResponse(e.Message);
+                }
+            }
+
+            if (request is GetTripsRequest)
+            {
+                Console.WriteLine("Received get trips request..");
+                
+                try
+                {
+                    Trip[] trips;
+                    lock (_server)
+                    {
+                        trips = _server.GetTrips();
+                    }
+
+                    return new GetAllTripsResponse(trips);
+                }
+                catch (Exception e)
+                {
+                    return new ErrorResponse(e.Message);
+                }
+            }
+
+            if (request is GetSearchedTripsRequest)
+            {
+                Console.WriteLine("Received get searched trips request..");
+                GetSearchedTripsRequest getSearchedTripsRequest = (GetSearchedTripsRequest)request;
+                FilterDTO filter = getSearchedTripsRequest.Filter;
+
+                try
+                {
+                    Trip[] searchedTrips;
+                    lock (_server)
+                    {
+                        searchedTrips = _server.GetSearchedTrips(filter.Landmark, filter.StratHour, filter.EndHour);
+                    }
+
+                    return new GetSearchedTripsResponse(searchedTrips);
+                }
+                catch (Exception e)
+                {
+                    return new ErrorResponse(e.Message);
+                }
+            }
+
+            if (request is LogoutRequest)
+            {
+                Console.WriteLine("Received logout request..");
+                LogoutRequest logoutRequest = (LogoutRequest)request;
+                User currentUser = logoutRequest.User;
+
+                try
+                {
+                    lock (_server)
+                    {
+                        _server.Logout(currentUser, this);
+                    }
+
+                    _connected = false;
+                    return new OkResponse();
+                }
+                catch (Exception e)
+                {
+                    return new ErrorResponse(e.Message);
+                }
+            }
+
+            return null;
+        }
+        
+        public virtual void Run()
+        {
+            while (_connected)
+            {
+                try
+                {
+                    object request = _formatter.Deserialize(_stream);
+                    object response = HandleRequest((IRequest)request);
+                    if (response != null)
+                    {
+                        SendResponse((IResponse) response);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                }
+
+                try
+                {
+                    Thread.Sleep(1000);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                }
+            }
+
+            try
+            {
+                _stream.Close();
+                _connection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
---
+++ C#/persistence/reservationRepo/ReservationDbRepository.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using log4net;
+using model;
+
+namespace persistence.reservationRepo;
+
+public class ReservationDbRepository : IReservationRepository
+{
+    private static readonly ILog Log = LogManager.GetLogger("ReservationDbRepository");
+    private IDictionary<string, string> _props;
+
+    public ReservationDbRepository(IDictionary<string, string> props)
+    {
+        Log.Info("Creating ReservationDbRepository...");
+        _props = props;
+    }
+    
+    public IEnumerable<Reservation> FindAll()
+    {
+        Log.InfoFormat("Entering FindAll() from ReservationDbRepository");
+        IDbConnection con = DbUtils.GetConnection(_props);
+        List<Reservation> reservations = new List<Reservation>();
+
+        using (var comm = con.CreateCommand())
+        {
+            comm.CommandText = "SELECT * FROM Reservations";
+
+            using (var resultSet = comm.ExecuteReader())
+            {
+                while (resultSet.Read())
+                {
+                    int id = resultSet.GetInt32(0);
+                    string clientName = resultSet.GetString(1);
+                    string phoneNumber = resultSet.GetString(2);
+                    int numberOfTickets = resultSet.GetInt32(3);
+                    int userId = resultSet.GetInt32(4);
+                    int tripId = resultSet.GetInt32(5);
+
+                    User agencyTravelUser = new User(null, null);
+                    agencyTravelUser.Id = userId;
+
+                    Trip trip = new Trip(null, null, new DateTime(), -1, -1);
+                    trip.Id = tripId;
+
+                    Reservation reservation = new Reservation(clientName, phoneNumber, numberOfTickets, agencyTravelUser, trip);
+                    reservation.Id = id;
+                    reservations.Add(reservation);
+                }
+            }
+        }
+        
+        Log.InfoFormat("Exiting FindAll() from ReservationDbRepository");
+        return reservations;
+    }
+    
+#nullable enable
+    public Reservation? Save(Reservation entity)
+    {
+        Log.InfoFormat("Entering Save() from ReservationDbRepository with value {0}", entity.ClientName);
+        IDbConnection con = DbUtils.GetConnection(_props);
+
+        using (var comm = con.CreateCommand())
+        {
+            comm.CommandText = "INSERT INTO Reservations(clientName, phoneNumber, numberOfTickets, idAgencyUser, idTrip) VALUES (@cN, @pN, @nT, @idUser, @idTrip)";
+            IDbDataParameter paramClientName = comm.CreateParameter();
+            paramClientName.ParameterName = "@cN";
+            paramClientName.Value = entity.ClientName;
+            comm.Parameters.Add(paramClientName);
+
+            IDbDataParameter paramPhoneNumber = comm.CreateParameter();
+            paramPhoneNumber.ParameterName = "@pN";
+            paramPhoneNumber.Value = entity.PhoneNumber;
+            comm.Parameters.Add(paramPhoneNumber);
+
+            IDbDataParameter paramNumberOfTickets = comm.CreateParameter();
+            paramNumberOfTickets.ParameterName = "@nT";
+            paramNumberOfTickets.Value = entity.NumberOfTickets;
+            comm.Parameters.Add(paramNumberOfTickets);
+
+            IDbDataParameter paramUserId = comm.CreateParameter();
+            paramUserId.ParameterName = "@idUser";
+            paramUserId.Value = entity.AgencyUser.Id;
+            comm.Parameters.Add(paramUserId);
+
+            IDbDataParameter paramTripId = comm.CreateParameter();
+            paramTripId.ParameterName = "@idTrip";
+            paramTripId.Value = entity.CurrentTrip.Id;
+            comm.Parameters.Add(paramTripId);
+
+            var result = comm.ExecuteNonQuery();
+            if (result == 0)
+            {
+                Log.Error("Error while trying to save reservation with clientName " + entity.ClientName);
+                return entity;
+            }
+        }
+        
+        Log.InfoFormat("Exiting Save() from ReservationDbRepository");
+        return null;
+    }
+
+    public Reservation? Update(Reservation entity)
+    {
+        return null;
+    }
+}
