7942be4cd0ecd585aa804de4b01006e5eb69f18a
Nistor Ioan-Razvan <149099252+razv11@users.noreply.github.com>
2024-06-09 17:29:15 +0300

    Add files via upload

---
+++ C#/services/ITravelAgencyServices.cs
@@ -0,0 +1,13 @@
+﻿using model;
+
+namespace services
+{
+    public interface ITravelAgencyServices
+    {
+        void Login(User user, ITravelAgencyObserver client);
+        void ReserveTrip(Reservation reservation);
+        void Logout(User user, ITravelAgencyObserver client);
+        Trip[] GetTrips();
+        Trip[] GetSearchedTrips(string landmark, int startHour, int endHour);
+    }
+}
---
+++ C#/services/TravelAgencyException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace services
+{
+    public class TravelAgencyException : Exception
+    {
+        public TravelAgencyException() : base() { }
+        public TravelAgencyException(string message) : base(message) { }
+        public TravelAgencyException(string message, Exception exception) : base(message, exception) { }
+    }
+}
