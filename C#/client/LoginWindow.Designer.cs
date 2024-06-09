08d5765aae6361ff317d0a180d03cd097a6d3da4
Nistor Ioan-Razvan <149099252+razv11@users.noreply.github.com>
2024-06-09 17:38:25 +0300

    Add files via upload

---
+++ C#/client/LoginWindow.Designer.cs
@@ -0,0 +1,200 @@
+﻿using System.ComponentModel;
+
+namespace client
+{
+    partial class LoginWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanelLogin = new System.Windows.Forms.TableLayoutPanel();
+            this.loginlabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanelLogin.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanelLogin
+            // 
+            this.tableLayoutPanelLogin.AutoSize = true;
+            this.tableLayoutPanelLogin.ColumnCount = 2;
+            this.tableLayoutPanelLogin.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelLogin.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelLogin.Controls.Add(this.loginlabel, 0, 0);
+            this.tableLayoutPanelLogin.Controls.Add(this.passwordLabel, 0, 2);
+            this.tableLayoutPanelLogin.Controls.Add(this.usernameLabel, 0, 1);
+            this.tableLayoutPanelLogin.Controls.Add(this.usernameTextBox, 1, 1);
+            this.tableLayoutPanelLogin.Controls.Add(this.passwordTextBox, 1, 2);
+            this.tableLayoutPanelLogin.Controls.Add(this.panel1, 1, 3);
+            this.tableLayoutPanelLogin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelLogin.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelLogin.Name = "tableLayoutPanelLogin";
+            this.tableLayoutPanelLogin.RowCount = 4;
+            this.tableLayoutPanelLogin.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tableLayoutPanelLogin.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanelLogin.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanelLogin.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelLogin.Size = new System.Drawing.Size(677, 431);
+            this.tableLayoutPanelLogin.TabIndex = 0;
+            // 
+            // loginlabel
+            // 
+            this.loginlabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.loginlabel.Font = new System.Drawing.Font("Bahnschrift", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginlabel.ForeColor = System.Drawing.Color.Black;
+            this.loginlabel.Location = new System.Drawing.Point(3, 0);
+            this.loginlabel.Name = "loginlabel";
+            this.loginlabel.Size = new System.Drawing.Size(332, 150);
+            this.loginlabel.TabIndex = 0;
+            this.loginlabel.Text = "Login form";
+            this.loginlabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.passwordLabel.Font = new System.Drawing.Font("Cambria", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLabel.ForeColor = System.Drawing.Color.Black;
+            this.passwordLabel.Location = new System.Drawing.Point(3, 236);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(332, 86);
+            this.passwordLabel.TabIndex = 2;
+            this.passwordLabel.Text = "Password";
+            this.passwordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.usernameLabel.Font = new System.Drawing.Font("Cambria", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameLabel.ForeColor = System.Drawing.Color.Black;
+            this.usernameLabel.Location = new System.Drawing.Point(3, 150);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(332, 86);
+            this.usernameLabel.TabIndex = 1;
+            this.usernameLabel.Text = "Username";
+            this.usernameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.usernameTextBox.BackColor = System.Drawing.Color.Silver;
+            this.usernameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.usernameTextBox.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameTextBox.Location = new System.Drawing.Point(407, 178);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(200, 30);
+            this.usernameTextBox.TabIndex = 3;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.passwordTextBox.BackColor = System.Drawing.Color.Silver;
+            this.passwordTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.passwordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTextBox.Location = new System.Drawing.Point(407, 264);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(200, 29);
+            this.passwordTextBox.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.loginButton);
+            this.panel1.Controls.Add(this.cancelButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(341, 325);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(333, 103);
+            this.panel1.TabIndex = 5;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.loginButton.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.Location = new System.Drawing.Point(102, 21);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(79, 38);
+            this.loginButton.TabIndex = 1;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cancelButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelButton.Location = new System.Drawing.Point(187, 21);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(79, 38);
+            this.cancelButton.TabIndex = 0;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // LoginWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(677, 431);
+            this.Controls.Add(this.tableLayoutPanelLogin);
+            this.Location = new System.Drawing.Point(15, 15);
+            this.MaximumSize = new System.Drawing.Size(850, 550);
+            this.MinimumSize = new System.Drawing.Size(410, 250);
+            this.Name = "LoginWindow";
+            this.Load += new System.EventHandler(this.LoginWindow_Load);
+            this.tableLayoutPanelLogin.ResumeLayout(false);
+            this.tableLayoutPanelLogin.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button loginButton;
+
+        private System.Windows.Forms.Panel panel1;
+
+        private System.Windows.Forms.TextBox passwordTextBox;
+
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.Label passwordLabel;
+
+        private System.Windows.Forms.Label usernameLabel;
+
+        private System.Windows.Forms.Label loginlabel;
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelLogin;
+
+        #endregion
+    }
+}
---
+++ C#/client/LoginWindow.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+using System.Windows.Forms;
+using model;
+
+namespace client
+{
+    public partial class LoginWindow : Form
+    {
+        private TravelAgencyClientController _controller;
+        public LoginWindow(TravelAgencyClientController ctrl)
+        {
+            InitializeComponent();
+            _controller = ctrl;
+        }
+
+        private void LoginWindow_Load(object sender, EventArgs e)
+        {
+            Text = @"Travel agency - Login";
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            string username = usernameTextBox.Text;
+            string password = passwordTextBox.Text;
+            try
+            {
+                _controller.Login(username, password);
+                TravelAgencyWindow mainWindow = new TravelAgencyWindow(_controller, new model.User(username, password));
+                mainWindow.Show();
+                Hide();
+                
+                usernameTextBox.Clear();
+                passwordTextBox.Clear();
+            }
+            catch (Exception ex)
+            {
+                string error = "Login error: " + ex.Message;
+                MessageBox.Show(null, error, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
