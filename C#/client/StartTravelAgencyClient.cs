4dca794a9432f7e0cf126b863f8d414776b129a0
Nistor Ioan-Razvan <149099252+razv11@users.noreply.github.com>
2024-06-09 17:39:30 +0300

    Add files via upload

---
+++ C#/client/StartTravelAgencyClient.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Configuration;
+using System.Windows.Forms;
+using networking.objectprotocol;
+using protobuff3;
+using services;
+
+namespace client
+{
+    static class StartTravelAgencyClient
+    {
+        private static readonly string DefaultHost = "127.0.0.1";
+        private static readonly int DefaultPort = 55555;
+        
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            string host;
+            try
+            {
+                host = ConfigurationManager.AppSettings["host"];
+            }
+            catch (Exception exception)
+            {
+                host = DefaultHost;
+                Console.WriteLine("Host property not set. Using default host: {0}", host);
+            }
+
+            string portStr;
+            int port;
+            try
+            {
+                portStr = ConfigurationManager.AppSettings["port"];
+                try
+                {
+                    port = int.Parse(portStr);
+                }
+                catch (Exception ex)
+                {
+                    port = DefaultPort;
+                    Console.WriteLine("Port property is not numeric. Using default port: {0}", port);
+                }
+            }
+            catch (Exception e)
+            {
+                port = DefaultPort;
+                Console.WriteLine("Port property not set. Using default port: {0}", port);
+            }
+
+            //ITravelAgencyServices server = new TravelAgencyServerObjectProxy(host, port);
+            ITravelAgencyServices server = new ProtoTravelAgencyProxy(host, port);
+            TravelAgencyClientController controller = new TravelAgencyClientController(server);
+            LoginWindow loginWindow = new LoginWindow(controller);
+            Application.Run(loginWindow);
+        }
+    }
+}
---
+++ C#/client/TravelAgencyClientController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using model;
+using services;
+
+namespace client
+{
+    public class TravelAgencyClientController : ITravelAgencyObserver
+    {
+        public event EventHandler<TravelAgencyUserEventArgs> UpdateEvent;
+        private readonly ITravelAgencyServices _server;
+        private model.User _currentUser;
+
+        public TravelAgencyClientController(ITravelAgencyServices server)
+        {
+            _server = server;
+            _currentUser = null;
+        }
+
+        public virtual void OnUserEvent(TravelAgencyUserEventArgs e)
+        {
+            if (UpdateEvent == null) return;
+            UpdateEvent(this, e);
+            Console.WriteLine(@"Update event called!");
+        }
+        
+        public void TripReserved(model.Reservation reservation)
+        {
+            Console.WriteLine(@"Trip reserved {0}", reservation);
+            TravelAgencyUserEventArgs args = new TravelAgencyUserEventArgs(TravelAgencyUserEvent.TripReserved, reservation);
+            OnUserEvent(args);
+        }
+        
+        public void Login(string username, string password)
+        {
+            model.User user = new model.User(username, password);
+            _server.Login(user, this);
+            _currentUser = user;
+            Console.WriteLine(@"Current user: {0}", username);
+        }
+        
+        public void ReserveTrip(string clientName, string phoneNumber, int noSeats, model.User user, model.Trip trip)
+        {
+            model.Reservation newReservation = new model.Reservation(clientName, phoneNumber, noSeats, user, trip);
+            _server.ReserveTrip(newReservation);
+        }
+
+        public model.Trip[] GetAllTrips()
+        {
+            return _server.GetTrips();
+        }
+
+        public model.Trip[] GetSearchedTrips(string landmark, int startHour, int endHour)
+        {
+            return _server.GetSearchedTrips(landmark, startHour, endHour);
+        }
+
+        public void Logout()
+        {
+            Console.WriteLine(@"Logout for {0}", _currentUser.Username);
+            _server.Logout(_currentUser, this);
+            _currentUser = null;
+        }
+    }
+}
